6492aa67451a95d6459cde0cdd50d7e08e0457d4
Mohit <mohitmayank1@gmail.com>
2021-01-30 13:06:03 +0530

    Create virtual_environment.sh

---
+++ virtual_environment.sh
@@ -0,0 +1,6 @@
+# Create and Activate Virtual environment
+# -----------------------------------------
+# Create the virtual environment in the current directory
+python -m venv projectnamevenv
+# activate the virtual environment - Linux
+.projectnamevenv\Scripts\activate
