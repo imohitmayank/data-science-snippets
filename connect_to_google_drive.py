a25debd3cf511e7463751780f918302b67d765b8
Mohit <mohitmayank1@gmail.com>
2021-02-24 21:43:00 +0530

    Create connect_to_google_drive.py

---
+++ connect_to_google_drive.py
@@ -0,0 +1,4 @@
+# Load the Drive helper and mount
+# Taken from: https://stackoverflow.com/questions/47744131/colaboratory-can-i-access-to-my-google-drive-folder-and-file
+from google.colab import drive
+drive.mount('/content/drive')
