544e594210264acc824669ce5f663a99134d6d6f
Mohit <mohitmayank1@gmail.com>
2021-03-08 02:17:12 +0530

    Update virtual_environment.sh

--- virtual_environment.sh
+++ virtual_environment.sh
@@ -2,5 +2,8 @@
 # -----------------------------------------
 # Create the virtual environment in the current directory
 python -m venv projectnamevenv
+# pick one of the below based on your OS (default: Linux)
 # activate the virtual environment - Linux
 .projectnamevenv\Scripts\activate
+# activate the virtual environment - windows
+# .\\projectnamevenv\\Scripts\\activate.bat
