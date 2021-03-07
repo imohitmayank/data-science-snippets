5429a2d7a0e85c10d19c0e9775bb76b9697eb603
Mohit <mohitmayank1@gmail.com>
2021-03-08 02:18:19 +0530

    Update python_packaging.sh

--- python_packaging.sh
+++ python_packaging.sh
@@ -1,3 +1,6 @@
+# clean the previous build files
 python setup.py clean --all
+# build the new distribution files
 python setup.py sdist bdist_wheel
+# upload the latest version to pypi
 twine upload --skip-existing dist/*
