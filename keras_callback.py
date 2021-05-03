0a512f1f42164db43aca885789b57ce0546a88fd
Mohit <mohitmayank1@gmail.com>
2021-05-03 22:56:24 +0530

    Create keras_callback.py

---
+++ keras_callback.py
@@ -0,0 +1,18 @@
+# import
+import keras
+
+# Step 1: Defining the callbacks
+#------------------------------
+# define early stopping callback
+earlystopping = keras.callbacks.EarlyStopping(monitor='loss', patience=10)
+
+# define model checkpoint callback
+checkpoint = ModelCheckpoint("checkpoint_model_{epoch:02d}.hdf5", monitor='loss', verbose=1, save_best_only=False, mode='auto', period=10)
+
+# Step 2: Assigning the callback
+#------------------------------
+# fit the model
+history = model.fit(train_data_gen, 
+#                     ....
+                    callbacks=[checkpoint, earlystopping]
+                   )
